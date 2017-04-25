290867e103c35f7f5ec4dd3e9054e22c7ddf9c31
Drag0n69 <jordan.zerathe@gmail.com>
2017-04-25 18:10:10 +0200

    Smeagol le gogol

--- questions.js
+++ questions.js
@@ -1,7 +1,7 @@
 const questions = [
 	{ q: "de quelle couleur est la veste que porte Teddyboy RSA ?",								answer: "verte",},
 	{ q: "A quel crew appartient Teddyboy ?",													answer: "RSA"},
-	{ q: "Qui est la personne sur cette photo ?",												answer: "Teddyboy RSA", 		img: "teddy.jpg",},
+	{ q: "Qui est la personne sur cette photo ?",												answer: "Teddyboy RSA", 		img: "images/teddy.jpg",},
 	{ q: "La reponse est B",																	answer: "B"},
 	{ q: "La reponse est C",																	answer: "C"},
 ];
